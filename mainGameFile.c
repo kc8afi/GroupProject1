3ce17094d1b06d74f54b146a90b3c96316e27e65
Austin Bower <bowera18@mycu.concord.edu>
2015-03-03 21:30:18 +0000

    Aesthetic fixes, timer fixes, and variable initializing. Maze Traversal function needs fixed, for some reason PrintMaze call doesn't work properly and the printed maze is skewed drastically

--- mainGameFile.c
+++ mainGameFile.c
@@ -7,6 +7,7 @@ Group Project 1 - Maze
 #include <stdio.h>
 #include <stdlib.h>
 #include <curses.h>
+#include <time.h>
 
 void PrintMaze(char array[][22], int rows, int cols);
 void StoreMaze(char array[][22], int rows, int cols, FILE *file);
@@ -14,7 +15,7 @@ int MazeTraversal(char array[][22], int rows, int cols, int timeLimit);
 
 int main()
 {
-    int mazeCols = 22;
+	int mazeCols = 22;
 	int mazeRows = 21;
 	//Open up the file, read in the text file data into an array, print the array, and close the file as we won't need it again
 	FILE *file1 = fopen("maze1.txt", "r");
@@ -22,71 +23,73 @@ int main()
 	StoreMaze(mazeOne, mazeRows, mazeCols, file1);
 	fclose(file1);
 
-    FILE *file2 = fopen("maze2.txt", "r");
+	FILE *file2 = fopen("maze2.txt", "r");
 	char mazeTwo[mazeRows][mazeCols];
 	StoreMaze(mazeTwo, mazeRows, mazeCols, file2);
 	fclose(file2);
 
-    FILE *file3 = fopen("maze3.txt", "r");
+	FILE *file3 = fopen("maze3.txt", "r");
 	char mazeThree[mazeRows][mazeCols];
 	StoreMaze(mazeThree, mazeRows, mazeCols, file2);
 	fclose(file2);
 
-    FILE *file4 = fopen("maze4.txt", "r");
+	FILE *file4 = fopen("maze4.txt", "r");
 	char mazeFour[mazeRows][mazeCols];
 	StoreMaze(mazeFour, mazeRows, mazeCols, file2);
 	fclose(file2);
 
-    FILE *file5 = fopen("maze5.txt", "r");
+	FILE *file5 = fopen("maze5.txt", "r");
 	char mazeFive[mazeRows][mazeCols];
 	StoreMaze(mazeFive, mazeRows, mazeCols, file2);
 	fclose(file2);
 
-    FILE *file6 = fopen("maze6.txt", "r");
+	FILE *file6 = fopen("maze6.txt", "r");
 	char mazeSix[mazeRows][mazeCols];
 	StoreMaze(mazeSix, mazeRows, mazeCols, file2);
 	fclose(file2);
 
-    FILE *file7 = fopen("maze7.txt", "r");
+	FILE *file7 = fopen("maze7.txt", "r");
 	char mazeSeven[mazeRows][mazeCols];
 	StoreMaze(mazeSeven, mazeRows, mazeCols, file2);
 	fclose(file2);
 
-    FILE *file8 = fopen("maze8.txt", "r");
+	FILE *file8 = fopen("maze8.txt", "r");
 	char mazeEight[mazeRows][mazeCols];
 	StoreMaze(mazeEight, mazeRows, mazeCols, file2);
 	fclose(file2);
 
-    FILE *file9 = fopen("maze9.txt", "r");
+	FILE *file9 = fopen("maze9.txt", "r");
 	char mazeNine[mazeRows][mazeCols];
 	StoreMaze(mazeNine, mazeRows, mazeCols, file2);
 	fclose(file2);
 
-    FILE *file10 = fopen("maze10.txt", "r");
+	FILE *file10 = fopen("maze10.txt", "r");
 	char mazeTen[mazeRows][mazeCols];
 	StoreMaze(mazeTen, mazeRows, mazeCols, file2);
 	fclose(file2);
 
-    //Initialize the screen, set the cursor to invisible, auto return values with getch, allow special characters, and don't show keyboard input
+	//Initialize the screen, set the cursor to invisible, auto return values with getch, allow special characters, and don't show keyboard input
 	initscr();
 	curs_set(0);
 	noecho();
 	keypad(stdscr, TRUE);
 	cbreak();
 
-    //trying to set up a timer for the maze. once it works, set up a switch case for different time limits depending on which maze is being called
+	//trying to set up a timer for the maze. once it works, set up a switch case for different time limits depending on which maze is being called
 	int timeLimit = 60;
-	if (MazeTraversal(mazeOne, mazeRows, mazeCols, timeLimit) != 0)
-    {
-        printw("You succeeded!\n");
-    }
+
+	int completionStatus = MazeTraversal(mazeOne, mazeRows, mazeCols, timeLimit);
+	if (completionStatus != 0)
+	{
+        	printw("You succeeded!\n");
+	}
 
 	getch();
 	endwin();
 	return 0;
 }
 
-void PrintMaze(char array[][22], int mazeRows, int cols)
+void PrintMaze(char array[][22], int rows, int cols)
 {
 	int counter1 = 0;
 	while (counter1 < rows)
@@ -104,9 +107,9 @@ void PrintMaze(char array[][22], int mazeRows, int cols)
 
 void StoreMaze(char array[][22], int rows, int cols, FILE *file)
 {
-    //go through a maze .txt file and store the values in an array
-    int counter1 = 0;
-    while (counter1 < rows)
+	//go through a maze .txt file and store the values in an array
+	int counter1 = 0;
+	while (counter1 < rows)
 	{
 		int counter2 = 0;
 		while (counter2 < cols)
@@ -120,26 +123,26 @@ void StoreMaze(char array[][22], int rows, int cols, FILE *file)
 
 int MazeTraversal(char array[][22], int rows, int cols, int timeLimit)
 {
-    //allow the user to traverse through the maze
-    int success = 0; //if zero, then the user failed. if nonzero, the user succeeded
+	//allow the user to traverse through the maze
+	int success = 0; //if zero, then the user failed. if nonzero, the user succeeded
 
-    //Create variables for starting and ending position and then figure out what they should be set to
-	int curRow;
-	int curCol;
-	int endRow;
-	int endCol;
+	//Create variables for starting and ending position and then figure out what they should be set to
+	int curRow = 0;
+	int curCol = 0;
+	int endRow = 0;
+	int endCol = 0;
 	int counter1 = 0;
 	while (counter1 < rows)
 	{
 		int counter2 = 0;
 		while (counter2 < cols)
 		{
-			if (mazeArray[counter1][counter2] == 'X')
+			if (array[counter1][counter2] == 'X')
 			{
 				curRow = counter1;
 				curCol = counter2;
 			}
-			if (mazeArray[counter1][counter2] == 'O')
+			if (array[counter1][counter2] == 'O')
 			{
 				endRow = counter1;
 				endCol = counter2;
@@ -150,25 +153,26 @@ int MazeTraversal(char array[][22], int rows, int cols, int timeLimit)
 		counter1++;
 	}
 	//Run the loop that will contain the maze movement operations
-	time_t startTime = time();
+	clock_t t;
+	t = clock();
 	while (1)
 	{
 		clear();
 		refresh();
 		printw("Use the arrow keys to navigate the X through the maze\n");
-		PrintMaze(mazeArray, rows, cols);
+		PrintMaze(array, rows, cols);
 		int input = getch();
 		switch (input)
 		{
 			case KEY_UP:
 				if (curRow > 1)
 				{
-					if (mazeArray[curRow-1][curCol] == ' ')
+					if (array[curRow-1][curCol] == ' ')
 					{
-						if (mazeArray[curRow-2][curCol] == ' ' || mazeArray[curRow-2][curCol] == 'O')
+						if (array[curRow-2][curCol] == ' ' || array[curRow-2][curCol] == 'O')
 						{
-							mazeArray[curRow][curCol] = ' ';
-							mazeArray[curRow-2][curCol] = 'X';
+							array[curRow][curCol] = ' ';
+							array[curRow-2][curCol] = 'X';
 							curRow = curRow - 2;
 						}
 					}
@@ -177,12 +181,12 @@ int MazeTraversal(char array[][22], int rows, int cols, int timeLimit)
 			case KEY_RIGHT:
 				if (curCol < 19)
 				{
-					if (mazeArray[curRow][curCol+1] == ' ')
+					if (array[curRow][curCol+1] == ' ')
 					{
-						if (mazeArray[curRow][curCol+2] == ' ' || mazeArray[curRow][curCol+2] == 'O')
+						if (array[curRow][curCol+2] == ' ' || array[curRow][curCol+2] == 'O')
 						{
-							mazeArray[curRow][curCol] = ' ';
-							mazeArray[curRow][curCol+2] = 'X';
+							array[curRow][curCol] = ' ';
+							array[curRow][curCol+2] = 'X';
 							curCol = curCol + 2;
 						}
 					}
@@ -191,12 +195,12 @@ int MazeTraversal(char array[][22], int rows, int cols, int timeLimit)
 			case KEY_DOWN:
 				if (curRow < 19)
 				{
-					if (mazeArray[curRow+1][curCol] == ' ')
+					if (array[curRow+1][curCol] == ' ')
 					{
-						if (mazeArray[curRow+2][curCol] == ' ' || mazeArray[curRow+2][curCol] == 'O')
+						if (array[curRow+2][curCol] == ' ' || array[curRow+2][curCol] == 'O')
 						{
-							mazeArray[curRow][curCol] = ' ';
-							mazeArray[curRow+2][curCol] = 'X';
+							array[curRow][curCol] = ' ';
+							array[curRow+2][curCol] = 'X';
 							curRow = curRow + 2;
 						}
 					}
@@ -205,12 +209,12 @@ int MazeTraversal(char array[][22], int rows, int cols, int timeLimit)
 			case KEY_LEFT:
 				if (curCol > 1)
 				{
-					if (mazeArray[curRow][curCol-1] == ' ')
+					if (array[curRow][curCol-1] == ' ')
 					{
-						if (mazeArray[curRow][curCol-2] == ' ' || mazeArray[curRow][curCol-2] == 'O')
+						if (array[curRow][curCol-2] == ' ' || array[curRow][curCol-2] == 'O')
 						{
-							mazeArray[curRow][curCol] = ' ';
-							mazeArray[curRow][curCol-2] = 'X';
+							array[curRow][curCol] = ' ';
+							array[curRow][curCol-2] = 'X';
 							curCol = curCol - 2;
 						}
 					}
@@ -223,18 +227,19 @@ int MazeTraversal(char array[][22], int rows, int cols, int timeLimit)
 		{
 			if (curCol == endCol)
 			{
-			    time_t endTime = time();
 				clear();
 				refresh();
-				PrintMaze(mazeArray, rows, cols);
+				PrintMaze(array, rows, cols);
 				printw("GAME OVER, YOU WIN!");
 				break;
 			}
 		}
 	}
-    if (startTime - endTime <= timeLimit)
-    {
-        success = 1;
-    }
-    return success;
+	t = clock() - t;
+	double timeTaken = ((double)t) / CLOCKS_PER_SEC;
+	if (timeTaken <= timeLimit)
+	{
+		success = 1;
+	}
+	return success;
 }
