2012a0daf5160e7b2413edabf78f6b2c37df3638
Austin Bower <bowera18@mycu.concord.edu>
2015-03-07 18:35:07 +0000

    Edited enemy text files to include enemy name and level. Added functions to store the enemy data and to print enemy data. Minor aesthetic and formatting changes

--- mainGameFile.c
+++ mainGameFile.c
@@ -9,8 +9,17 @@ Group Project 1
 #include <curses.h>
 #include <time.h>
 
+struct Enemy
+{
+	int power;
+	char name[10];
+	char array[10][11];
+};
+
 void PrintMaze(char array[][22], int rows, int cols, WINDOW *window);
 void StoreMaze(char array[][22], int rows, int cols, FILE *file);
+void PrintEnemy(struct Enemy in, int rows, int cols, WINDOW *window);
+void StoreEnemy(struct Enemy* in, int rows, int cols, FILE *file);
 int MazeTraversal(char array[][22], int rows, int cols, int timeLimit, WINDOW *window);
 int Random(int Max);
 void DiceRoll(WINDOW *diceWindow);
@@ -19,56 +28,91 @@ int main()
 {
 	int mazeCols = 22;
 	int mazeRows = 21;
+	int enemyCols = 11;
+	int enemyRows = 10;
 	//Open up the file, read in the text file data into an array, print the array, and close the file as we won't need it again
 	FILE *file1 = fopen("maze1.txt", "r");
 	char mazeOne[mazeRows][mazeCols];
 	StoreMaze(mazeOne, mazeRows, mazeCols, file1);
 	fclose(file1);
-
 	FILE *file2 = fopen("maze2.txt", "r");
 	char mazeTwo[mazeRows][mazeCols];
 	StoreMaze(mazeTwo, mazeRows, mazeCols, file2);
 	fclose(file2);
-
 	FILE *file3 = fopen("maze3.txt", "r");
 	char mazeThree[mazeRows][mazeCols];
 	StoreMaze(mazeThree, mazeRows, mazeCols, file3);
 	fclose(file3);
-
 	FILE *file4 = fopen("maze4.txt", "r");
 	char mazeFour[mazeRows][mazeCols];
 	StoreMaze(mazeFour, mazeRows, mazeCols, file4);
 	fclose(file4);
-
 	FILE *file5 = fopen("maze5.txt", "r");
 	char mazeFive[mazeRows][mazeCols];
 	StoreMaze(mazeFive, mazeRows, mazeCols, file5);
 	fclose(file5);
-
 	FILE *file6 = fopen("maze6.txt", "r");
 	char mazeSix[mazeRows][mazeCols];
 	StoreMaze(mazeSix, mazeRows, mazeCols, file6);
 	fclose(file6);
-
 	FILE *file7 = fopen("maze7.txt", "r");
 	char mazeSeven[mazeRows][mazeCols];
 	StoreMaze(mazeSeven, mazeRows, mazeCols, file7);
 	fclose(file7);
-
 	FILE *file8 = fopen("maze8.txt", "r");
 	char mazeEight[mazeRows][mazeCols];
 	StoreMaze(mazeEight, mazeRows, mazeCols, file8);
 	fclose(file8);
-
 	FILE *file9 = fopen("maze9.txt", "r");
 	char mazeNine[mazeRows][mazeCols];
 	StoreMaze(mazeNine, mazeRows, mazeCols, file9);
 	fclose(file9);
-
 	FILE *file10 = fopen("maze10.txt", "r");
 	char mazeTen[mazeRows][mazeCols];
 	StoreMaze(mazeTen, mazeRows, mazeCols, file10);
 	fclose(file10);
+
+	//Read in enemy graphics files
+	FILE *enemy1 = fopen("enemy1.txt", "r");
+	struct Enemy enemyOne;
+	StoreEnemy(&enemyOne, enemyRows, enemyCols, enemy1);
+	fclose(enemy1);
+	FILE *enemy2 = fopen("enemy2.txt", "r");
+	struct Enemy enemyTwo;
+	StoreEnemy(&enemyTwo, enemyRows, enemyCols, enemy2);
+	fclose(enemy2);
+	FILE *enemy3 = fopen("enemy3.txt", "r");
+	struct Enemy enemyThree;
+	StoreEnemy(&enemyThree, enemyRows, enemyCols, enemy3);
+	fclose(enemy3);
+	FILE *enemy4 = fopen("enemy4.txt", "r");
+	struct Enemy enemyFour;
+	StoreEnemy(&enemyFour, enemyRows, enemyCols, enemy4);
+	fclose(enemy4);
+	FILE *enemy5 = fopen("enemy5.txt", "r");
+	struct Enemy enemyFive;
+	StoreEnemy(&enemyFive, enemyRows, enemyCols, enemy5);
+	fclose(enemy5);
+	FILE *enemy6 = fopen("enemy6.txt", "r");
+	struct Enemy enemySix;
+	StoreEnemy(&enemySix, enemyRows, enemyCols, enemy6);
+	fclose(enemy6);
+	FILE *enemy7 = fopen("enemy7.txt", "r");
+	struct Enemy enemySeven;
+	StoreEnemy(&enemySeven, enemyRows, enemyCols, enemy7);
+	fclose(enemy7);
+	FILE *enemy8 = fopen("enemy8.txt", "r");
+	struct Enemy enemyEight;
+	StoreEnemy(&enemyEight, enemyRows, enemyCols, enemy8);
+	fclose(enemy8);
+	FILE *enemy9 = fopen("enemy9.txt", "r");
+	struct Enemy enemyNine;
+	StoreEnemy(&enemyNine, enemyRows, enemyCols, enemy9);
+	fclose(enemy9);
+	FILE *enemy10 = fopen("enemy10.txt", "r");
+	struct Enemy enemyTen;
+	StoreEnemy(&enemyTen, enemyRows, enemyCols, enemy10);
+	fclose(enemy10);
 
 	//Initialize the screen, set the cursor to invisible, auto return values with getch, allow special characters, and don't show keyboard input
 	initscr();
@@ -98,9 +142,11 @@ int main()
 	{
 		wprintw(mazeWindow, "You failed!\n");
 	}
-	wprintw(enemyWindow, "ENEMY HERE");
+
+	PrintEnemy(enemyNine, enemyRows, enemyCols, enemyWindow);
 	wrefresh(enemyWindow);
-	dice_roll(diceWindow);
+
+	DiceRoll(diceWindow);
 	wrefresh(enemyWindow);
 	wrefresh(diceWindow);
 
@@ -140,6 +186,50 @@ void StoreMaze(char array[][22], int rows, int cols, FILE *file)
 		while (counter2 < cols)
 		{
 			array[counter1][counter2] = fgetc(file);
+			counter2++;
+		}
+		counter1++;
+	}
+}
+
+void PrintEnemy(struct Enemy in, int rows, int cols, WINDOW *window)
+{
+	//print out the enemy's power, name, and graphics array
+	wprintw(window, "Enemy level: %d\n", in.power);
+	int counter1 = 0;
+	wprintw(window, "Name: ");
+	while (counter1 < 10)
+	{
+		wprintw(window, "%c", in.name[counter1]);
+		counter1 ++;
+	}
+	counter1 = 0;
+	while (counter1 < rows)
+	{
+		int counter2 = 0;
+		while (counter2 < cols)
+		{
+			wprintw(window, "%c", in.array[counter1][counter2]);
+			counter2++;
+		}
+		counter1++;
+	}
+	wprintw(window, "\n\n");
+}
+
+void StoreEnemy(struct Enemy* in, int rows, int cols, FILE *file)
+{
+	//read in the enemy text file and store the first int as the enemy's power, the next bit of text as it's name, and the rest as the 2d graphic array
+	in->power = fgetc(file) - '0';
+	in->power = in->power +5;//add 5 to the level to increase difficulty
+	fgets(in->name, 11, file);
+	int counter1 = 0;
+	while (counter1 < rows)
+	{
+		int counter2 = 0;
+		while (counter2 < cols)
+		{
+			in->array[counter1][counter2] = fgetc(file);
 			counter2++;
 		}
 		counter1++;
